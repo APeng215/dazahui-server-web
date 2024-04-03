d7f782b2ee8d01cc2d25316eacadcd230e299d04
A_Peng215 <976310604@qq.com>
2024-04-03 17:02:07 +0800

    Fix url bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/icon.png',
 
   // Set the production url of your site here
-  url: 'https://APeng215.github.io/dazahui-server-web',
+  url: 'https://APeng215.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
